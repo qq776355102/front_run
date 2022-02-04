b7e2a7b255a425e53e82633ca530fd06cb0e054a
qq776355102 <41848512+qq776355102@users.noreply.github.com>
2022-02-05 01:20:13 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,28 @@
+const PRIVATE_KEY=''//ze客户的
+
+
+
+// const TOKEN_ADDRESS='0x12bb890508c125661e03b09ec06e404bc9289040';
+
+
+// const TOKEN_ADDRESS='0x89366229ad6aace698d1454dbf1323cb45eb575f';
+
+// const TOKEN_ADDRESS='0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82';//cake
+// const TOKEN_ADDRESS='0xe9e7cea3dedca5984780bafc599bd69add087d56';//busd
+
+const TOKEN_ADDRESS='0xE9c7a827a4bA133b338b844C19241c864E95d75f';//fsg
+
+
+// const TOKEN_ADDRESS="0x672f96b77c2a5da280ef1fa8d884e9243cce3abe";//HBM
+
+// const TOKEN_ADDRESS= "0x14E8BCd053e68A22f239b9e9bEaD87932465D245";
+const AMOUNT=0.2
+const LEVEL=3
+// const LEVEL=0.1 //千分之一
+
+module.exports = {
+    PRIVATE_KEY,
+    TOKEN_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
